0e512711c21fe74e04e2e48389161bdb036f8388
gatlingmcgee <rzman03@aol.com>
2025-05-17 22:03:28 -0500

    W02 Code Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,64 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Checks if queue contains both data and priority
+    // Expected Result: Returns the item with the highest priority first
+    // Defect(s) Found: Originally did not remove the item from the queue.
+    // Added "_queue.RemoveAt(highPriorityIndex);" line
+    public void HighestPriorityItem()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var queue = new PriorityQueue();
+        queue.Enqueue("A", 1);
+        queue.Enqueue("B", 3);
+        queue.Enqueue("C", 2);
+
+        var result = queue.Dequeue();
+
+        Assert.AreEqual("B", result);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Checks if queue contains multiple items with the same priority
+    // Expected Result: Returns the first item queued with the highest priority first
+    // Defect(s) Found: Needed to grab the first item with equal priority
+    // Removed the "=" from "if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)" line
+    public void MultipleHighPriorityItems()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var queue = new PriorityQueue();
+        queue.Enqueue("A", 2);
+        queue.Enqueue("B", 5);
+        queue.Enqueue("C", 5);
+
+        var result = queue.Dequeue();
+
+        Assert.AreEqual("B", result);
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Queue is called when empty
+    // Expected Result: An InvalidOperationException is thrown
+    // Defect(s) Found: No defect
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void EmptyQueueThrowsException()
+    {
+        var queue = new PriorityQueue();
+        queue.Dequeue();
+    }
+
+    [TestMethod]
+    // Scenario: Multiple items are added and removed multiple times
+    // Expected Result: Items are removed in descending order according to priority
+    // Defect(s) Found: Original did not remove the item from the queue
+    // Added "_queue.RemoveAt(highPriorityIndex);" line
+    public void RemoveCorrectItemsInOrder()
+    {
+        var queue = new PriorityQueue();
+        queue.Enqueue("X", 1);
+        queue.Enqueue("Y", 3);
+        queue.Enqueue("Z", 2);
+
+        Assert.AreEqual("Y", queue.Dequeue());
+        Assert.AreEqual("Z", queue.Dequeue());
+        Assert.AreEqual("X", queue.Dequeue());
+    }
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,11 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Finite turns were removed one turn too early,
+    // resulting in people with 1 turn left not being re-added to the queue.
+    // Added else if statement where person.Turns == 1
+
+
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +47,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: Same issue as last test case: Turns were removed one turn too early,
+    // resulting in people with 1 turn left not being re-added to the queue.
+    // Added else if statement where person.Turns == 1
+
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +92,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with 0 turns were not re-added to the queue 
+    // because the original code only re-added them if Turns > 1.
+    // Added if statement where person.Turns <= 0
+
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +126,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Same issue as last test case: People with 0 turns were not re-added to the queue 
+    // because the original code only re-added them if Turns > 1. (in this case, these numbers were negative)
+    // Added if statement where person.Turns <= 0
+
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +156,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: No defect
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
